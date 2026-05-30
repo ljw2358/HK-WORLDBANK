27f44d2b15b6c4d25c159545e353e7b703cad1a1
Jaewan Lee (Hyekong) <ljw2358@gmail.com>
2026-05-30 15:32:44 +0900

    Update hk_engine.js

--- hk_engine.js
+++ hk_engine.js
@@ -1,13 +1,1 @@
-window.HK_WorldBank_activate = function() {
-    console.log("엔진 가동 성공!");
-};
 
-function sendMoney() { pay(); }
-function payment() { payment(); }
-function deposit() { deposit(); }
-function stake() { stake(); }
-function claimReward() { claimReward(); }
-function claimUBI() { claimUBI(); }
-function approveSwap() { approveSwap(); }
-function getPiBalance() { getPiBalance(); }
-function executeSwap() { executeSwap(); }
