a5b23d43b5a59ba7c93eddbac06bcb74eda15b82
Jaewan Lee (Hyekong) <ljw2358@gmail.com>
2026-05-31 12:30:55 +0900

    Update hk_engine.js

--- hk_engine.js
+++ hk_engine.js
@@ -81,4 +81,37 @@ const HK_Engine = {
 
 // 시스템 실행
 HK_Engine.bootAll();
+/**
+ * HK-WorldBank 확장형 엔진
+ * 기존 9개 모듈 유지 및 새로운 모듈 추가 가능 구조
+ */
 
+const HK_Engine = {
+    // 기존 9개 덩어리 보존
+    modules: [
+        { id: 1, name: '송금' }, { id: 2, name: '결제' }, { id: 3, name: '예치' },
+        { id: 4, name: '보상' }, { id: 5, name: '기본소득' }, { id: 6, name: '스왑' },
+        { id: 7, name: 'PI 수량' }, { id: 8, name: 'DEX 스왑' }, { id: 9, name: 'LP 스테이킹' }
+    ],
+
+    // 새로운 모듈을 엔진에 추가하는 기능 (삭제 없이 확장만 가능)
+    addModule: function(name) {
+        const newId = this.modules.length + 1;
+        this.modules.push({ id: newId, name: name });
+        console.log(`%c[엔진 확장] ${name} 모듈이 추가되었습니다. (현재 총 ${newId}개)`, "color: #ff9900;");
+    },
+
+    // 전체 가동
+    bootAll: function() {
+        console.log("%c=== HK-WorldBank 통합 엔진 가동 시작 ===", "color: #00ffff; font-size: 16px;");
+        this.modules.forEach(m => {
+            console.log(`%c[가동] ${m.id}번 모듈: ${m.name} 작동 중...`, "color: #00ff00;");
+        });
+        console.log("%c=== 모든 덩어리 안정화 완료 ===", "color: #00ffff; font-size: 16px;");
+    }
+};
+
+// 페이지 로드 시 엔진 가동
+window.onload = function() {
+    HK_Engine.bootAll();
+};
