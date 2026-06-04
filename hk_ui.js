9e2b0e43e62cc4b09232353a6129ced56e337c33
Jaewan Lee (Hyekong) <ljw2358@gmail.com>
2026-06-05 08:17:18 +0900

    Update hk_ui.js

--- hk_ui.js
+++ hk_ui.js
@@ -1,1 +1,23 @@
+// hk_ui.js - HK-WorldBank UI 컨트롤러
+const HK_UI = {
+    // 엔진 기능 매핑
+    init: function() {
+        console.log("[UI] 시스템 버튼 매핑 시작");
+    },
+
+    // 9개 덩어리 호출 함수
+    exec: function(module, action) {
+        if (HK_Engine[module] && HK_Engine[module][action]) {
+            HK_Engine[module][action]();
+        } else {
+            console.error("해당 기능을 찾을 수 없습니다.");
+        }
+    }
+};
+
+// 페이지 로드 시 UI 초기화
+window.onload = function() {
+    HK_UI.init();
+    HK_Engine.bootAll(); // 엔진 가동
+};
 
